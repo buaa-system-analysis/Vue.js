d554220caa84854fcd0398a1e32974963f2f6d15
SkyLee <skylee@SkyLeedeMacBook-Pro.local>
2019-05-18 21:15:50 +0800

    subscribe

--- frontend/src/views/Scholar.vue
+++ frontend/src/views/Scholar.vue
@@ -55,7 +55,37 @@
       </el-tab-pane>
 
       <el-tab-pane label="我关注的学者" name="third">
-
+        <ul style="text-align: left; padding: 0 0 0 0">
+          <li v-for="(item, index) in subscribeList" :key="index"
+              style="line-height: 150%; padding-bottom: 30px; width: 50%; display: inline-block">
+            <el-row>
+              <el-col :span="4">
+                <i class="el-icon-s-custom" style="color: darkorange; font-size: 60px"></i>
+              </el-col>
+              <el-col :span="14">
+                <el-row style="color: black; font-size: 18px">{{item['scholar']}}</el-row>
+                <el-row style="color: black">{{item['org']}}</el-row>
+                <el-row>
+                  <el-col :span="6" style="color: darkgray">发表文章:</el-col>
+                  <el-col :span="6" style="color: black">{{item['pub']}}</el-col>
+                  <el-col :span="6" style="color: darkgray">被引次数:</el-col>
+                  <el-col :span="6" style="color: black">{{item['citation']}}</el-col>
+                </el-row>
+                <el-row>
+                  <el-col :span="6" style="color: darkgray">研究领域:</el-col>
+                  <el-col :span="18" style="color: black">
+                    <span  v-for="f in item['field']" :key="f" style="padding-right: 5px">{{f}}</span>
+                  </el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="6">
+                <el-button style="background-color: darkorange; color: white"
+                           icon="el-icon-check" round>已关注</el-button>
+                <el-button style="background-color: darkorange; color: white; margin: 5px 0 0 0" round>前往查看</el-button>
+              </el-col>
+            </el-row>
+          </li>
+        </ul>
       </el-tab-pane>
 
     </el-tabs>
@@ -121,6 +151,22 @@ export default {
           citation: 10698,
           field: ['NLP', 'CV', 'ML']
         }
+      ],
+      subscribeList: [
+        {
+          scholar: 'Xinhang Li',
+          org: 'Beihang University',
+          pub: 618,
+          citation: 10698,
+          field: ['NLP', 'CV', 'ML']
+        },
+        {
+          scholar: 'Xinhang Li',
+          org: 'Beihang University',
+          pub: 618,
+          citation: 10698,
+          field: ['NLP', 'CV', 'ML']
+        }
       ]
     }
   },
@@ -134,7 +180,6 @@ export default {
       this.reload()
     },
     getScholarList () {
-      alert('get it!')
     }
   },
   mounted: function () {
